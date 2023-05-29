fd58eaa3121e4a93ca961cabb2ea4664a5e31690
JUSTIN NIRVANTIO <62611939+Justinnirvan04@users.noreply.github.com>
2023-05-30 04:34:52 +0700

    Add files via upload
    Aplikasi Website untuk memperkenalkan Wisata dan Kuliner Pati
    Yang dbuat leh justin Nirvantio

---
+++ justwisatapati/index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1" />
+    <title>JUSTPATIKU</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,500;0,600;1,300&display=swap"
+    rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="css/owl.carousel.min.css">
+    <link type="text/css" rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="arrow">
+        <i class="fa fa-arrow-up"></i>
+    </div>
+    <nav>
+        <div class="container">
+            <div class="menu-par">
+                <div class="logo-par">
+                    <a href="index.html">
+                        <h2>JUSTPATIKU</h2>
+                    </a>
+                </div>
+                <div class="nav">
+                    <ul>
+                        <li><a class="menu-hover" href="index.html">Home</a></li>
+                    </ul>
+                </div>
+                <div id="toggle-btn">
+                    <i class="fa fa-bars"></i>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="container">
+            <div class="header-info-par">
+                <h1 class="typewrite" data-period="2000"
+                data-type='["SELAMAT DATANG DI HALAMAN JUSTPATIKU"]'>
+            </h1>
+            <p>sebuah kabupaten di Provinsi Jawa Tengah, Indonesia. Ibu kotanya adalah Pati. Kabupaten ini terkenal dengan semboyan Pati Bumi Mina Tani. Penduduk kabupaten Pati berjumlah 1.324.188 jiwa pada akhir tahun 2020, dan 1.348.397 jiwa di tahun 2021.</p>
+            <a href="#" class="a">SELENGKAPNYA</a>
+        </div>
+        <div class="video">
+            <video id="video" autoplay loop muted>
+                <source src="images/profpati.mp4" type="video/mp4" />
+            </video>
+        </div>
+    </div>
+</header>
+<section id="welcome-text">
+    <div class="container">
+        <h2>Berbagai macam wisata di pati</h2>
+        <p>Terdapat banyak wisata di pati antara lain sebagai berikut</p>
+    </div>
+</section>
+<section id="slides-par">
+    <div class="container">
+        <div class="slides">
+            <div class="slide_1 slide_2">
+                <div class="slide-info">
+                    <p>SENDANG</p>
+                </div>
+            </div>
+            <div class="slide_1 slide_3">
+                <div class="slide-info">
+                    <p>GUA</p>
+                </div>
+            </div>
+            <div class="slide_1 slide_4">
+                <div class="slide-info">
+                    <p>WADUK</p>
+                </div>
+            </div>
+            <div class="slide_1 slide_5">
+                <div class="slide-info">
+                    <p>AIR TERJUN</p>
+                </div>
+            </div>
+            <div class="slide_1 slide_6">
+                <div class="slide-info">
+                    <p>ALUN-ALUN</p>
+                </div>
+            </div>
+            <div class="slide_1 slide_7">
+                <div class="slide-info">
+                    <p>PULAU</p>
+                </div>
+            </div>
+            <div class="slide_1 slide_8">
+                <div class="slide-info">
+                    <p>PANTAI</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="book">
+    <div class="container">
+        <div class="book-content-par">
+            <div class="book-counter">
+                <h3>Buku Tamu </h3>
+                <div id="clockdiv">
+                    <div>
+                        <span class="days"></span>
+                        <div class="smalltext">Days</div>
+
+                    </div>
+                    <div>
+                        <span class="hours"></span>
+                        <div class="smalltext">Hours</div>
+
+                    </div>
+                    <div>
+                        <span class="minutes"></span>
+                        <div class="smalltext">Minutes</div>
+
+                    </div>
+                    <div>
+                        <span class="seconds"></span>
+                        <div class="smalltext">Seconds</div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="book-form">
+                <form>
+                    <input type="text" id="fname" name="firstname" placeholder="Your Name...">
+                    <input type="text" id="lname" name="lastname" placeholder="Your Last Name...">
+                    <select name="country" id="country">
+                        <option value="Indonesia">Indonesia</option>
+                    </select>
+                    <textarea name="subject" id="subject" placeholder="Komentar...."
+                    style="height: 200px;"></textarea>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="gallery">
+    <div class="container">
+        <div class="row">
+            <div align="center" class="filter-btn-par">
+                <button class="filter-button" data-filter="all">SEMUA</button>
+                <button class="filter-button" data-filter="alam">ALAM</button>
+                <button class="filter-button" data-filter="kuliner">KULINER</button>
+                <button class="filter-button" data-filter="religi">RELIGI</button>
+            </div>
+            <div class="filter-gal-par">
+                <div class="gallery-item filter alam">
+                    <img src="images/alamreligikulinerpati/Waduk-Gunung-Rowo.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter alam">
+                    <img src="images/alamreligikulinerpati/Air-terjun-Tretes.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter alam">
+                    <img src="images/alamreligikulinerpati/argapesona.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter religi">
+                    <img src="images/alamreligikulinerpati/jangkung.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter religi">
+                    <img src="images/alamreligikulinerpati/makamronggokusumo.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter religi">
+                    <img src="images/alamreligikulinerpati/agenggerang.jfif" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter kuliner">
+                    <img src="images/alamreligikulinerpati/nasi-gandul.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="gallery-item filter kuliner">
+                    <img src="images/alamreligikulinerpati/bandeng-presto.jpg" alt="" class="img-responsive">
+                </div> 
+                <div class="gallery-item filter kuliner">
+                    <img src="images/alamreligikulinerpati/telur-lurik.jpg" alt="" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="water-slider">
+    <div class="container">
+        <h3>WISATA TERPOPULER DI PATI</h3>
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="owl-img-par">
+                    <img src="images/slider/waduk-Seloromo.jpg" alt="" class="img-responsive" />
+                    <div class="owl-img-info">
+                        <h3>Waduk Seloromo</h3>
+                        <p>PATI</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="owl-img-par">
+                    <img src="images/slider/Pantai-Idola-Banyutowo.jpg" alt="" class="img-responsive" />
+                    <div class="owl-img-info">
+                        <h3>Pantai Idola</h3>
+                        <p>PATI</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="owl-img-par">
+                    <img src="images/slider/Air-Terjun-Niagara-Grodo.jpg" alt="" class="img-responsive" />
+                    <div class="owl-img-info">
+                        <h3>AT Niagara Grodo</h3>
+                        <p>PATI</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="owl-img-par">
+                    <img src="images/slider/Candi-Kayen.jpg" alt="" class="img-responsive" />
+                    <div class="owl-img-info">
+                        <h3>Candi Kayen</h3>
+                        <p>PATI</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="owl-img-par">
+                    <img src="images/slider/Lorotan-Semar.jpg" alt="" class="img-responsive" />
+                    <div class="owl-img-info">
+                        <h3>Lorotan Semar</h3>
+                        <p>PATI</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="blog">
+    <div class="container">
+        <h3>TENTANG</h3>
+        <div class="blog-par">
+            <div class="blog-content">
+                <!-- one div for image and one for info -->
+                <div class="blog-img-par">
+                    <img src="images/alamreligikulinerpati/nasi-gandul.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="blog-info-par">
+                    <ul>
+                        <li>JUSTPATIKU</li>
+                    </ul>
+                    <h4>NASI GANDUL</h4>
+                    <p>Nasi gandul ini salah satu makanan khas di kota Pati itu sendiri.</p>
+                </div>
+            </div>
+            <div class="blog-content">
+                <div class="blog-img-par">
+                    <img src="images/slider/Gua-Pancur-Jimbaran.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="blog-info-par">
+                    <ul>
+                        <li>JUSTPATIKU</li>
+                    </ul>
+                    <h4>GUA PANCUR JIMBARAN</h4>
+                    <p>Gua Pancur Jimbaran, yang memiliki banyak mitos dan cerita sejarah.</p>
+                </div>
+            </div>
+        </div>
+        <div class="blog-par">
+            <div class="blog-content">
+                <div class="blog-img-par">
+                    <img src="images/slider/Desa-Wisata-Jolong-Pati.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="blog-info-par">
+                    <ul>
+                        <li>JUSTPATIKU</li>
+                    </ul>
+                    <h4>Desa Wisata Jolong Pati</h4>
+                    <p>dengan pemandangan alam yang indah tidak sepi dari pengunjung.</p>
+                </div>
+            </div>
+            <div class="blog-content">
+                <div class="blog-img-par">
+                    <img src="images/alamreligikulinerpati/kacang-khas-pati.jpg" alt="" class="img-responsive">
+
+                </div>
+                <div class="blog-info-par">
+                    <ul>
+                        <li>JUSTPATIKU</li>
+                    </ul>
+                    <h4>Kacang Khas Pati</h4>
+                    <p>kacang dari Pati ini Dibuat langsung dari tangan pabrik kecil rumahan.......... </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="container">
+        <div class="footer-par">
+            <div class="footer-1 ">
+                <h3 class="footer-logo-h">JUSTPATIKU</h3>
+                <span class="footer-social-overlap footer-social-connect">
+                </span>
+                <span class="footer-social-overlap footer-social-icons-wrapper">
+                  <a href="https://www.facebook.com/profile.php?id=100009488632531" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fa fa-facebook"></i></a>
+                  <a href="http://instagram.com/just_yourmay" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="fa fa-instagram"></i></a>
+                  <a href="https://plus.google.com/+justintio/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a>
+                  <a href="https://api.whatsapp.com/send/?phone=6287732623701&text&app_absent=0" class="generic-anchor" target="_blank" title="Whatsapp" itemprop="significantLink"><i class="fa fa-whatsapp"></i></a>
+              </span>
+          </div>
+          <div class="footer-1 footer-info">
+            <h3>UAS DASKOM</h3>
+            <ul>
+                <li><a href="">JUSTIN NIRVANTIO</a></li>
+                <li><a href="">A11.2021.13426</a></li>
+                <li><a href="">4109</a></li>
+                <li><a href="">20/01/2022</a></li>
+            </ul>
+        </div>
+        <div class="footer-1 footer-info">
+            <h3>Bagai Mana tampilan Web Saya?</h3>
+            <ul>
+                <li><a href="">Beri masukan anda</a></li>
+            </ul>
+        </div>
+        <div class="footer-1 footer-info">
+            <h3>Hubungi</h3>
+            <ul>
+                <li><a href="">+6287732623701</a></li>
+                <li><a href="">111202113426@mhs.dinus.ac.id</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<p class="copyright">Copyright © 20/01/2022 Dibuat oleh Justin Nirvantio</p>
+</footer>
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/extrenaljq.js"></script>
+</body>
+
+</html>
